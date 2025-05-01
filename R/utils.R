65644bda47c0bb06597a7998998a79774eb79730
zpneal <zpneal@msu.edu>
2025-05-01 11:32:02 -0400

    Add files via upload

--- R/utils.R
+++ R/utils.R
@@ -4,7 +4,7 @@
   packageStartupMessage("Cite: Neal, Z. P., Neal, J. W., Cappella, E., and Dockerty, M. (2025). KOLaide: A tool for selecting")
   packageStartupMessage("      key opinion leaders under practical constraints. GitHub. https://github.com/zpneal/KOLaid/")
   packageStartupMessage("      ")
-  packageStartupMessage("Help: type vignette(\"KOLaide\"); email zpneal@msu.edu; github zpneal/KOLaide")
+  packageStartupMessage("Help: email zpneal@msu.edu; github zpneal/KOLaide")
   packageStartupMessage("Beta: type devtools::install_github(\"zpneal/KOLaide\", ref = \"devel\")")
 }
 
